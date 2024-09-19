2fc82104055f4a2fbe6829d2803f1c6429f9af74
hwyz_leo <hwyz.leo.ye@gmail.com>
2024-09-19 23:32:18 +0800

    功能: 增加操作系统枚举类

---
+++ framework-commons-starter/src/main/java/net/hwyz/iov/cloud/tsp/framework/commons/enums/Os.java
@@ -0,0 +1,17 @@
+package net.hwyz.iov.cloud.tsp.framework.commons.enums;
+
+/**
+ * 操作系统枚举类
+ *
+ * @author hwyz_leo
+ */
+public enum Os {
+
+    /** 安卓 **/
+    ANDROID,
+    /** 苹果 **/
+    IOS,
+    /** 鸿蒙 **/
+    HARMONYOS;
+
+}
